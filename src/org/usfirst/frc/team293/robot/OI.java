a4dbb8a426d7b09a22d410251f3d6e7aebd9831d
FRC Team 293 Hopewell, NJ <293.First@gmail.com>
2017-02-04 16:57:55 -0500

    OI Stuff for Joysticks

--- src/org/usfirst/frc/team293/robot/OI.java
+++ src/org/usfirst/frc/team293/robot/OI.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team293.robot;
 
+import org.usfirst.frc.team293.robot.commands.FeederFoward;
+import org.usfirst.frc.team293.robot.commands.FeederStop;
 import org.usfirst.frc.team293.robot.commands.GearFlapDown;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -16,13 +18,30 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 
 public class OI {
-	private Joystick joy = new Joystick(0);
-
+	///////////instantiate joysticks
+	 private static Joystick joy1 = new Joystick(0);
+	 private static Joystick joy2=new Joystick(1);
+	 public static Joystick launchpad=new Joystick(2);
+	 public static Joystick launchpad2=new Joystick(3);
+	 
 	public OI() {
-	}
-
-	public Joystick getJoystick() {
-		return joy;
+		JoystickButton padOne=new JoystickButton(launchpad,1);
+		JoystickButton padTwo=new JoystickButton(launchpad,6);
+		JoystickButton padThree=new JoystickButton(launchpad,8);
+		JoystickButton padFour=new JoystickButton(launchpad,10);
+		JoystickButton padFive=new JoystickButton(launchpad,11);
+		JoystickButton padSix=new JoystickButton(launchpad,2);
+		JoystickButton padSeven=new JoystickButton(launchpad,3);
+		JoystickButton padEight=new JoystickButton(launchpad,5);
+		JoystickButton padNine=new JoystickButton(launchpad,4);
+		
+		JoystickButton twoWaySwitch=new JoystickButton(launchpad,11);
+		
+		twoWaySwitch.whileHeld(new FeederFoward());
+		padTwo.whenReleased(new FeederStop());
+		//padThree.whenPressed(new Sh);
+		
+				
 	}
 }
 
